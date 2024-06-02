bc4075e0403b5273f19b0db50241e42ef950b90c
KILLERTIAN <omsharma050322@gmail.com>
2024-06-02 14:36:44 +0530

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-     <div className="m-[30px] flex flex-col items-center ">
+     <div className="m-[30px] flex flex-col items-center backdrop-blur-md">
      <Navbar/>
      <Hero/>
+     <Features/>
      </div>
   )
 }
