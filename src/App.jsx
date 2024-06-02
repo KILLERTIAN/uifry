daaac6ed4074ce0e0a7b3fbe0d3bf6c17bed9bfb
KILLERTIAN <omsharma050322@gmail.com>
2024-06-02 18:25:26 +0530

    advantages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Why from './components/Why'
 
 function App() {
   return (
-     <div className=" flex flex-col items-center backdrop-blur-md">
+     <div className=" flex flex-col items-center backdrop-blur-md ">
      <Navbar/>
      <Hero/>
      <Features/>
+     <Why/>
      </div>
   )
 }
